67592bc1c3ec294cf3157f1d5ea6e78a160d2391
eco14cvo <eco14cvo@student.lu.se>
2016-05-24 10:46:29 +0200

    Added a comment.

--- first_testing.R
+++ first_testing.R
@@ -5,15 +5,18 @@
 library("bbmle")
 
 ### Load data ###
+### Data accessed from Yahoo Finance ###
 x<-read.csv("./Data/sp500.csv")
 attach(x)
+
 ### Data management ###
 x$Date<-as.Date(x$Date)
+
 ### Sort by date ###
 x<-x[order(x$Date),]
+
 ### Drop unnessary data ###
 x<-x[,c(1,7)]
-
 
 
 ### Normal dist. ###
