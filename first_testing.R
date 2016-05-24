fc1f8651eef3f6a61824568cabe9fb067cc01b95
eco14cvo <eco14cvo@student.lu.se>
2016-05-24 10:32:39 +0200

    Getting started.

---
+++ first_testing.R
@@ -0,0 +1,23 @@
+### Testing script ###
+
+### Load libraries ###
+### Likelihood-function ###
+library("bbmle")
+
+### Load data ###
+x<-read.csv("./Data/sp500.csv")
+attach(x)
+### Data management ###
+x$Date<-as.Date(x$Date)
+### Sort by date ###
+x<-x[order(x$Date),]
+### Drop unnessary data ###
+x<-x[,c(1,7)]
+
+
+
+### Normal dist. ###
+mylike<-function(mu=0, sigma=1){
+  -sum(stats::dnorm(x[,2],mean=mu, sd=sigma, log=TRUE))
+}
+
